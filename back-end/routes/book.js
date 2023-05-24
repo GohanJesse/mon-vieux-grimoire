2eaeaf3b6291b46bed8869251b37a5a85044faf3
Jessy <jessegodais@gmail.com>
2023-05-25 00:42:42 +0200

    avancé sur les routes, ne manque plus que la route pour les ratings / vérification de l'appartenance de l'utilisateur faite pour les routes POST et PUT / gestion des fichiers utilisateur faite, manque l'optimisation des images avec Sharp / étapes 1.1 et 1.3 terminée

--- back-end/routes/book.js
+++ back-end/routes/book.js
@@ -8,8 +8,9 @@ const bookCtrl = require('../controllers/book');
 
 router.get('/:id', auth, bookCtrl.getOneBook);
 router.get("/", auth, bookCtrl.getAllBook);
+router.get('/bestrating', auth, bookCtrl.getBestRating);
 router.post("/", auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, bookCtrl.modifyBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.modifyBook);
 
 module.exports = router;
